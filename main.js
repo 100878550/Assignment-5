869a71b52734599f292ef7f8dff69a7278664ed9
100878550 <ethan.smallwood@dcmail.ca>
2025-04-11 19:56:20 -0400

    buncha changes

--- main.js
+++ main.js
@@ -5,16 +5,14 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
-  let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
-    showHideBtn.textContent = 'Hide comments';
-    commentWrapper.style.display = 'block';
-  } else {
-    showHideBtn.textContent = 'Show comments';
-    commentWrapper.style.display = 'none';
-  }
+showHideBtn.onclick = function () {
+  const isExpanded = showHideBtn.getAttribute('aria-expanded') === 'true';
+
+  showHideBtn.setAttribute('aria-expanded', String(!isExpanded));
+  showHideBtn.textContent = isExpanded ? 'Show comments' : 'Hide comments';
+  commentWrapper.style.display = isExpanded ? 'none' : 'block';
 };
+
 
 // functionality for adding a new comment via the comments form
 
